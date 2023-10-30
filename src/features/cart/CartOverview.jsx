7258d9f6bd279001cd19af40d50787e8e21bc027
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-10-30 18:01:51 +0200

    adding fetching orders by ID.

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -4,8 +4,8 @@ function CartOverview() {
   return (
     <div>
       <p>
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>X pizzas</span>
+        <span> 💲XX</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,47 +1,22 @@
-// Test ID: IIDSAT
+// Test ID : IIDSAT || CQE92U
 
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
+export async function loader({ params }) {
+  const order = await getOrder(params.orderId);
+  // console.log(order);
+  return order;
+}
 
 function Order() {
+  const order = useLoaderData();
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -61,7 +36,7 @@ function Order() {
 
         <div>
           {priority && <span>Priority</span>}
-          <span>{status} order</span>
+          <span>: {status} order</span>
         </div>
       </div>
 
@@ -75,8 +50,8 @@ function Order() {
       </div>
 
       <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>Pizza Price: {formatCurrency(orderPrice)}</p>
+        {priority && <p>Priority Price : {formatCurrency(priorityPrice)}</p>}
         <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
     </div>
