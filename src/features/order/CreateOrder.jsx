d20d98ba951b0c062c88664475f02918eb3abb9f
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-11-08 09:54:45 +0200

    Styling cart, order form.Adding responsive design.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -43,16 +43,19 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST" action="/order/new">
-        <div>
-          <label>First Name</label>
-          <div>
+        <div
+          className="mb-5 flex flex-col gap-2
+        sm:flex-row sm:items-center"
+        >
+          <label className="sm:basis-40">First Name</label>
+          <div className="grow">
             <input
-              className="input-field"
+              className="input-field w-full"
               placeholder="write your name here..."
               type="text"
               name="customer"
@@ -61,29 +64,40 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
+        <div
+          className="mb-5 flex flex-col gap-2
+          sm:flex-row sm:items-center"
+        >
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
             <input
-              className="input-field"
+              className="input-field w-full"
               placeholder="write your phone number here..."
               type="tel"
               name="phone"
               required
             />
+            {formErrors?.phone && (
+              <p
+                className="
+              mt-2 rounded-md bg-red-100 p-2
+              text-xs text-red-700
+              "
+              >
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div
+          className="mb-5 flex flex-col gap-2
+          sm:flex-row sm:items-center"
+        >
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
             <input
-              className="w-full rounded-full border 
-              border-stone-200 px-4 py-2 text-sm transition-all
-              duration-300 placeholder:text-stone-500
-              focus:outline-none focus:ring
-              focus:ring-teal-400 md:px-6 md:py-3 "
+              className="input-field w-full"
               type="text"
               name="address"
               placeholder="write your address here..."
@@ -92,7 +106,7 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-teal-400 focus:outline-none
             focus:ring focus:ring-teal-400 focus:ring-offset-2"
@@ -102,7 +116,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
